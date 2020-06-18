409afbddcaf24e2ce7743d1c2aca275f88ca99fe
Tapan <tapanmanu2000@gmail.com>
2020-06-18 17:33:13 +0530

    first phase completed

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -20,8 +21,17 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
         val dice = Dice(6)
         val diceroll = dice.roll()
-        val dispText : TextView = findViewById(R.id.textView)
-        dispText.text = diceroll.toString()
+        val diceImage : ImageView = findViewById(R.id.imageView)
+        val drawing = when(diceroll){
+            1->R.drawable.dice_1
+            2->R.drawable.dice_2
+            3->R.drawable.dice_3
+            4->R.drawable.dice_4
+            5->R.drawable.dice_5
+            else ->R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawing)
+        diceImage.contentDescription = diceroll.toString()
     }
 }
 class Dice(val numSides:Int) {
